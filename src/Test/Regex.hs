05b068a8dc0facc8bb3c2fe8aa6c24f5103c456b
Erik Stevenson <eriknstevenson@gmail.com>
2018-02-28 13:04:39 -0500

    Extend examples, document functions

--- src/Test/Regex.hs
+++ src/Test/Regex.hs
@@ -7,54 +7,61 @@ import Test.QuickCheck
 import Text.Regex.PCRE.Heavy
 
 
+-- |A source of strings containing symbols of a certain alphabet
 type Alphabet = Gen String
 
+-- |A Property which needs to be given an Alphabet to evaluate 
 type RegexProperty = Alphabet -> Property
 
+-- |Constructs an alphabet with the given characters.
+-- Words produced by this alphabet will consist only of these characters.
 alphabetOf :: String -> Alphabet
 alphabetOf alphabet = 
   listOf (elements alphabet)
 
-
+-- |Verify that a Regex matches strings with a given property.
 checkRegex :: String -> Alphabet -> RegexProperty -> Spec
 checkRegex desc alphabet regexProp = 
   prop desc $ regexProp alphabet
 
-
+-- |Verify multiple Regex properties at the same time.
 checkRegexMulti :: String -> Alphabet -> [RegexProperty] -> Spec
 checkRegexMulti desc alphabet regexProps = 
   prop desc $ conjoin $ map (\f -> f alphabet) regexProps
 
-
+-- |Make a claim that a Regex will match strings with a given property.
 satisfies :: Regex -> (String -> Bool) -> RegexProperty
 satisfies regex predicate alphabet =
   forAll alphabet $ \w ->
     w =~ regex === predicate w
 
-
+-- |Make a claim that multiple Regexes will all match strings which satisfy a 
+-- given property.
 allSatisfy :: [Regex] -> (String -> Bool) -> RegexProperty
 allSatisfy regex predicate alphabet =
   forAll alphabet $ \w ->
     conjoin $ map (\r -> w =~ r === predicate w) regex
 
-
+-- |Assesses whether two Regexes act identically when provided same input 
+-- strings.
 isEquivalentTo :: Regex -> Regex -> RegexProperty
 isEquivalentTo a b alphabet =
   forAll alphabet $ \w ->
     (w =~ a) === (w =~ b)
 
-
+-- |Assesses whether multiple Regexes act identically when provided same input 
+-- strings.
 allEquivalent :: [Regex] -> RegexProperty
 allEquivalent regexes alphabet = 
   forAll alphabet $ \w ->
     conjoin $ map (w =~) regexes
 
-     
+-- |Returns whether given string does not contain given substring.
 doesNotContain :: Eq a => [a] -> [a] -> Bool
-doesNotContain s x = 
-  not (x `isInfixOf` s)
+doesNotContain string substring = 
+  not (substring `isInfixOf` string)
 
-
+-- |Returns whether given string does not have given suffix.
 doesNotEndWith :: Eq a => [a] -> [a] -> Bool
-doesNotEndWith s x = 
-  not (x `isSuffixOf` s)
+doesNotEndWith string suffix = 
+  not (suffix `isSuffixOf` string)
--- test/RegexSpec.hs
+++ test/RegexSpec.hs
@@ -2,7 +2,10 @@
 
 module RegexSpec (spec) where
 
+import Data.Functor
+import Data.List
 import Test.Hspec
+import Text.ParserCombinators.ReadP
 import Text.Regex.PCRE.Heavy
 
 import Test.Regex
@@ -10,65 +13,104 @@ import Test.Regex
 spec :: Spec
 spec = do
 
-  {-
-  -}
-  describe "Equivalency" $ do
-
-    let alphabet = alphabetOf "01"
-        regexA = [re|^(0|1)*$|]
-        regexB = [re|^(0|1)*(0|1)*$|]
-
-    checkRegex "test" alphabet $
-      regexA `isEquivalentTo` regexB
-
-  {-
-  -}
-  describe "Example 1" $ do
+  describe "Verifying regex matches language with property" $ do
     
-    let alphabet = alphabetOf "01"
-        regex = [re|^(0|(10))*$|]
+
+    describe "Example 1" $ do
+      let alphabet = alphabetOf "01"
+          regex = [re|^(0|(10))*$|]
+      
+      checkRegex "All 1s are immediately followed by a 0." alphabet $
+        regex `satisfies` \w ->
+          case length w of 
+            0 -> True
+            1 -> w /= "1"
+            _ -> w `doesNotContain` "11" && w `doesNotEndWith` "1"
+
+
+    describe "Example 2" $ do
+
+      let alphabet = alphabetOf "ab"
+          regex = [re|^b*(a|bb+)*b*$|]
+
+      checkRegex "Word does not have aba as substring" alphabet $
+        regex `satisfies` \w ->
+          w `doesNotContain` "aba"
+
+
+    describe "Example 3" $ do
+
+      let alphabet = alphabetOf "ab"
+          regex = [re|^(a|ba|bba)*(b|bb|\z)$|]
+
+      checkRegex "Word does not contain more than 2 consecutive bs" alphabet $
+        regex `satisfies` \w ->
+          w `doesNotContain` "bbb"
     
-    checkRegex "All 1s are immediately followed by a 0." alphabet $
-      regex `satisfies` \w ->
-        case length w of 
-          0 -> True
-          1 -> w /= "1"
-          _ -> w `doesNotContain` "11" && w `doesNotEndWith` "1"
+
+    describe "Example 4" $ do
+
+      let alphabet = alphabetOf "ab"
+          regex = [re|^((b*ab*(b*ab*ab*)*b*)|(a*ba*(a*ba*ba*)*a*))$|]
+
+      checkRegex "Word contains odd number of As or odd number of Bs" alphabet $
+        regex `satisfies` \w ->
+          let occurrences = map length . group . sort $ w
+          in any odd occurrences
+
+  describe "Equivalency of regular expressions" $ do
+
+    describe "Example 1" $ do
+      let alphabet = alphabetOf "01"
+          regexA = [re|^(01|0)*0$|]
+          regexB = [re|^0(10|0)*$|]
+
+      checkRegex "(01|0)*0 is equivalent to 0(10|0)*" alphabet $
+        regexA `isEquivalentTo` regexB
+          
+
+    describe "Example 2 - Verifying 3 regular expressions are all equivalent" $ do
+
+      let alphabet = alphabetOf "ab"
+          regexA = [re|^(a|b)*b*$|]
+          regexB = [re|^(a*|ba*)*$|]
+          regexC = [re|^(b*(a|\z)b*)*$|]
+
+      checkRegex "All of the regexes are equivalent" alphabet $
+        allEquivalent [regexA, regexB, regexC] 
+
+
+    describe "Example 3" $ do
+
+      let alphabet = alphabetOf "ab"
+          regexA = [re|^(ba)+(a*b*|a*)$|]
+          regexB = [re|^(ba)*ba+(b*|\z)$|]
+
+      checkRegex "(ba)+(a*b*|a*) is equivalent to (ba)*ba+(b*|\\z)" alphabet $
+        regexA `isEquivalentTo` regexB
+
+  describe "Verifying with parser combinator" $
       
-      
-  {-
-  -}
-  describe "Example 2 - Equivalency" $ do
+      describe "Example 1" $ do
 
-    let alphabet = alphabetOf "ab"
-        regexA = [re|^(a|b)*b*$|]
-        regexB = [re|^(a*|ba*)*$|]
-        regexC = [re|^(b*(a|\z)b*)*$|]
+        let alphabet = alphabetOf "ab"
+            regex = [re|^b(bb)*(aa)*$|]
+            
+            validator :: String -> Bool
+            validator input = not (null (parser input))
 
-    checkRegexMulti "All of the regexes are equivalent" alphabet 
-      [ allEquivalent [regexA, regexB, regexC] 
-      , allSatisfy [regexA, regexB, regexC] $ const True
-      ]
+            parser :: ReadS ()
+            parser = readP_to_S $
+              char 'b' 
+              *> many (pairOf 'b') 
+              *> many (pairOf 'a')
+              *> eof
+              $> ()
 
-  {-
-  -}
-  describe "Example 3" $ do
-
-    let alphabet = alphabetOf "ab"
-        regex = [re|^b*(a|bb+)*b*$|]
-
-    checkRegex "Word does not have aba as substring" alphabet $
-      regex `satisfies` \w ->
-        w `doesNotContain` "aba"
+        checkRegex "A language of strings containing an even number of A's, an odd number of B's, and not containing the substring ab" alphabet $
+          regex `satisfies` validator
       
 
-  {-
-  -}
-  describe "Example 4" $ do
-
-    let alphabet = alphabetOf "ab"
-        regex = [re|^(a|ba|bba)*(b|bb|\z)$|]
-
-    checkRegex "Word does not contain more than 2 consecutive bs" alphabet $
-      regex `satisfies` \w ->
-        w `doesNotContain` "bbb"
+pairOf :: Char -> ReadP String
+pairOf c =
+  string [c,c]
